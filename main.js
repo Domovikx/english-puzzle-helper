3c03cdd2ba0afe575ee7b2ffdc98499c961313e2
Ilya Ivanouski <domovikx@gmail.com>
2020-02-07 17:40:38 +0300

    feat: add getWords()

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 (function main() {
   const textarea = document.getElementById("textarea");
+  const wordsElement = document.getElementById("words");
 
-  changeHeight(textarea);
+  refreshTextarea(textarea);
 
-  textarea.addEventListener("click", () => changeHeight(textarea));
-  textarea.addEventListener("keyup", () => changeHeight(textarea));
+  textarea.addEventListener("click", refreshTextarea);
+  textarea.addEventListener("keyup", refreshTextarea);
+
+  function refreshTextarea() {
+    changeHeight(textarea);
+    const words = getWords(textarea);
+    renderWords(words, wordsElement);
+  }
 })();
 
-function changeHeight(element = document.getElementById("textarea")) {
+function changeHeight(element) {
   element.style.height = "0px";
   element.style.height = element.scrollHeight + "px";
+}
+
+function getWords(element) {
+  const words = element.value
+    .toLowerCase()
+    .replace(/[^A-Za-zА-Яа-яЁё]/g, " ")
+    .split(" ")
+    .filter(word => word != "");
+  const uniqueWords = [...new Set(words)].join("\n");
+  return uniqueWords;
+}
+
+function renderWords(words, wordsElement) {
+  wordsElement.innerHTML = words;
 }
