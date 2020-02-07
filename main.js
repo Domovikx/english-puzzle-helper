40ce2c764e1603b988d7b4d7ee1ed847a13aa5fa
Ilya Ivanouski <domovikx@gmail.com>
2020-02-07 18:35:48 +0300

    feat: ready to refactor

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
+const state = {
+  words
+};
+
 (function main() {
   const textarea = document.getElementById("textarea");
-  const wordsElement = document.getElementById("words");
 
   refreshTextarea(textarea);
 
   textarea.addEventListener("click", refreshTextarea);
   textarea.addEventListener("keyup", refreshTextarea);
-
-  function refreshTextarea() {
-    changeHeight(textarea);
-    const words = getWords(textarea);
-    renderWords(words, wordsElement);
-  }
 })();
 
-function changeHeight(element) {
+function refreshTextarea() {
+  const wordsElement = document.getElementById("words");
+  changeHeightTextarea();
+  const words = getWords(textarea.value);
+  state.words = words;
+  renderWords(words, wordsElement);
+}
+
+function changeHeightTextarea() {
+  const element = document.getElementById("textarea");
   element.style.height = "0px";
   element.style.height = element.scrollHeight + "px";
 }
 
-function getWords(element) {
-  const words = element.value
+function getWords(value) {
+  const words = value
     .toLowerCase()
     .replace(/[^A-Za-zА-Яа-яЁё]/g, " ")
     .split(" ")
@@ -31,4 +37,23 @@ function getWords(element) {
 
 function renderWords(words, wordsElement) {
   wordsElement.innerHTML = words;
+}
+
+function copyToClipboard() {
+  navigator.clipboard.writeText(state.words).catch(err => {
+    console.log("Something went wrong", err);
+  });
+}
+
+function pasteFromClipboard() {
+  navigator.clipboard
+    .readText()
+    .then(text => {
+      const textarea = document.getElementById("textarea");
+      textarea.value = text;
+      refreshTextarea();
+    })
+    .catch(err => {
+      console.log("Something went wrong", err);
+    });
 }
